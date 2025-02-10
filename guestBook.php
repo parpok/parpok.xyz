8faae2c10a6306c5d06e773b52435289aea16422
Patryk <me@parpok.xyz>
2025-02-10 14:19:31 +0100

    I think I fucked up here

--- guestBook.php
+++ guestBook.php
@@ -81,25 +81,6 @@
         <?php
         $db = new mysqli("localhost", "web", "", "parpokxyz");
 
-        $turnstile_secret     = $_ENV["TurnStyle_Secret"];
-        $turnstile_response   = $_POST['cf-turnstile-response'];
-        $url                  = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
-        $post_fields          = "secret=$turnstile_secret&response=$turnstile_response";
-        
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        
-        $response_data = json_decode($response);
-        if ($response_data->success != 1) {
-            header("Location: /");
-            exit;
-        }
-
-
         if ($db->connect_error) {
             die("Connection failed: " . $db->connect_error);
         }
