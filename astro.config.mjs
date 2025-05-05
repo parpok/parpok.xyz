3b89687d3bf3a80a4eaa8e51445164a914290690
Patryk <me@parpok.xyz>
2025-05-05 19:35:06 +0200

    Add docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://parpok.xyz",
   integrations: [sitemap()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
