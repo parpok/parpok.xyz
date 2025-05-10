baa09b88791dc1524bd3b64bf703838b5c4d631a
Patryk <me@parpok.xyz>
2025-05-10 18:23:49 +0200

    FIX MISTAKES WITH FILE DUPLICATION

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,18 +2,35 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-
-  const takes = await getCollection("thinks")
+  const thoughts = await getCollection("thinks");
 
   return rss({
+    // `<title>` field in output xml
     title: "Pat thinks",
-    description: "A collection of Pat's thoughts that can vary but might be seen as unhinged.",
+    // `<description>` field in output xml
+    description:
+      "A collection of Pats thoughts that can vary but might be seen as unhinged.",
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#site
     site: context.site,
-    items: takes.map((take) => ({
-      title: take.data.title,
-      pubDate: take.data.pubDate,
-      link: `/thinks/${take.id}`,
-      content: take.body
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: thoughts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      link: `/thinks/${post.url}/`.toLocaleLowerCase(),
+      pubDate: post.data.date,
     })),
+    customData: `<language>en-gb</language>
+   <image>
+      <url>https://parpok.xyz/avatar.png</url>
+      <title>Pat thinks</title>
+      <link>https://parpok.xyz/</link>
+      <width>32</width>
+      <height>32</height>
+      <description>Your site icon</description>
+    </image>
+    `,
+    // fuck the USA
   });
 }
